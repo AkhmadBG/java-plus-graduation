f805bdcead01c373f824bff0f90924950c89f4c5
AkhmadBG <akhmadbakir@yandex.ru>
2026-03-02 16:20:40 +0300

    Refactor api gateway routs

--- core/interaction/src/main/java/ru/practicum/ewm/core/interaction/feignclient/adm/AdminCategoryFeignClient.java
+++ core/interaction/src/main/java/ru/practicum/ewm/core/interaction/feignclient/adm/AdminCategoryFeignClient.java
@@ -3,6 +3,6 @@ package ru.practicum.ewm.core.interaction.feignclient.adm;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.ewm.core.interaction.apiinterface.adm.AdminCategoryOperations;
 
-@FeignClient(name = "categories", contextId = "AdminCategory", path = "/admin")
+@FeignClient(name = "events", contextId = "AdminCategory", path = "/admin/categories")
 public interface AdminCategoryFeignClient extends AdminCategoryOperations {
 }
--- core/interaction/src/main/java/ru/practicum/ewm/core/interaction/feignclient/priv/PrivateCommentFeignClient.java
+++ core/interaction/src/main/java/ru/practicum/ewm/core/interaction/feignclient/priv/PrivateCommentFeignClient.java
@@ -3,6 +3,6 @@ package ru.practicum.ewm.core.interaction.feignclient.priv;
 import org.springframework.cloud.openfeign.FeignClient;
 import ru.practicum.ewm.core.interaction.apiinterface.priv.PrivateCommentOperations;
 
-@FeignClient(name = "comments", contextId = "PrivateComment", path = "")
+@FeignClient(name = "comments", contextId = "PrivateComment", path = "/")
 public interface PrivateCommentFeignClient extends PrivateCommentOperations {
 }
